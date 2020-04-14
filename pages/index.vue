bbdbf86c1ed75f8bfa0e39fa5a5f169b8c112928
Will Holland <snowy.the.crow@gmail.com>
2020-04-14 13:54:53 +0700

    Initial commit
    A simple example of how to use this package with Nuxt. Based on a simple Bulma
    template

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <translate>Hello World</translate>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
